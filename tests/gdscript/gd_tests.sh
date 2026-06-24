17b2b662225b65b14182ea5113b762089945d0d5
Aiden Jeffrey <aiden.jeffrey@mlb.com>
2026-06-24 18:28:13 +0100

    gdscript: Move schema test files

--- tests/gdscript/gd_tests.sh
+++ tests/gdscript/gd_tests.sh
@@ -5,31 +5,33 @@ set -eu
 pushd "$(dirname $0)" >/dev/null
 test_dir="$(pwd)"
 gen_code_dir=${test_dir}/generated
-gen_ts_code_dir=${test_dir}/ts_check/src/generated
-# gen_py_code_dir=${test_dir}/py_check/src/generated
-data_dir=${test_dir}/data
+schema_dir=${test_dir}/data/schemas
 out_dir=${test_dir}/out
 
 mkdir -p "${out_dir}"
 mkdir -p "${gen_code_dir}"
-mkdir -p "${gen_ts_code_dir}"
+
+# gen_ts_code_dir=${test_dir}/ts_check/src/generated
+# mkdir -p "${gen_ts_code_dir}"
 
 pushd "../.."
 
-./flatc --gdscript --gen-object-api -o "${gen_code_dir}" "${data_dir}"/simple.fbs
-./flatc --gdscript --gen-object-api -o "${gen_code_dir}" "${data_dir}"/kitchen_sink.fbs
-./flatc --gdscript --gen-object-api --gen-all -o "${gen_code_dir}" "${data_dir}"/wincludes/base.fbs
-./flatc --ts --gen-object-api --ts-no-import-ext -o "${gen_ts_code_dir}" "${data_dir}"/simple.fbs
-./flatc --ts --gen-object-api --ts-no-import-ext -o "${gen_ts_code_dir}" "${data_dir}"/kitchen_sink.fbs
+./flatc --gdscript --gen-object-api -o "${gen_code_dir}" "${schema_dir}"/simple.fbs
+./flatc --gdscript --gen-object-api -o "${gen_code_dir}" "${schema_dir}"/kitchen_sink.fbs
+./flatc --gdscript --gen-object-api --gen-all -o "${gen_code_dir}" "${schema_dir}"/wincludes/base.fbs
+
+# NB: uncomment to generate reference bindings and test binaries using ts generator
+# ./flatc --ts --gen-object-api --ts-no-import-ext -o "${gen_ts_code_dir}" "${schema_dir}"/simple.fbs
+# ./flatc --ts --gen-object-api --ts-no-import-ext -o "${gen_ts_code_dir}" "${schema_dir}"/kitchen_sink.fbs
 
 popd
 
-pushd "ts_check"
+# pushd "ts_check"
 
-yarn start
+# yarn start
 
-popd
+# popd
 
-godot -s --headless tests.gd
+godot -s --headless tests/main.gd "$@"
 
-echo "SUCCESS!"
+echo "TESTS PASS, GD SUCCESS!"
